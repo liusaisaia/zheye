1cb7348e2da1c958ded5068dfb1f935757024c83
l1009470126 <1009470126@qq.com>
2020-10-10 22:33:53 +0800

    提交

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1></h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'ColumnList'
+})
+</script>
+
+<style>
+
+</style>
